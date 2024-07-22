2b3bdfa1cb1b640e7cb598a6460064289edd9b1f
ttaeyoung <taeyoung.lee.tpv@one-line.com>
2024-07-22 09:15:34 +0900

    jojojo

--- namabiru/read_csv.py
+++ namabiru/read_csv.py
@@ -12,14 +12,10 @@ def get_df_data(file_path, csv_path):
     csv_way = Xlsx2csv(file_path, outputencoding="utf-8").convert(csv_path)
     # pandas로 csv 파일 읽기
     df = pd.read_csv(csv_path)[2:]
-    selected = ['E2E_UID', 'Ocean_Priority', 'Trade', 'Sub_Trade', 'Service_Lane', 'Cargo_Cutoff_Time']
+    selected = []
     # selected = ['E2E UID','Ocean Priority','Trade','Sub Trade','Service Lane','Cargo Cutoff Time']
     df = df.rename(columns={
-        'E2E UID': 'E2E_UID',
-        'Ocean Priority': 'Ocean_Priority',
-        'Sub Trade': 'Sub_Trade',
-        'Service Lane': 'Service_Lane',
-        'Cargo Cutoff Time': 'Cargo_Cutoff_Time'
+        
     })
     new_df = df[selected]
 
@@ -30,7 +26,7 @@ def get_df_data(file_path, csv_path):
         data = {}
         for idx, row in new_df.iterrows():
             key = row['E2E_UID']
-            values = row[['Ocean_Priority','Trade', 'Sub_Trade','Service_Lane','Cargo_Cutoff_Time']]
+            values = row[]
             data[key] = values
         redis_server_main(data)
     
